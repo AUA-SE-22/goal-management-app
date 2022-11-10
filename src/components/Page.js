8cf522b50c223ac2b7068887d60fcd09ce574115
Zara <zaruhitshitoyan@gmail.com>
2022-11-10 20:49:22 +0400

    add page component

---
+++ src/components/Page.js
@@ -0,0 +1,27 @@
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+// next
+import Head from 'next/head';
+// @mui
+import { Box } from '@mui/material';
+
+const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
+  <>
+    <Head>
+      <title>{`${title} | Goal App`}</title>
+      {meta}
+    </Head>
+
+    <Box ref={ref} {...other} sx={{ m: 3 }}>
+      {children}
+    </Box>
+  </>
+));
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string,
+  meta: PropTypes.node,
+};
+
+export default Page;
