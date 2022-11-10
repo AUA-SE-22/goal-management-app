54cdf609cebca7f1afaaf48f1e8f7c2289decbfc
Zara <zaruhitshitoyan@gmail.com>
2022-11-10 20:50:03 +0400

    update home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
+// @mui
+import { Box, Typography } from '@mui/material';
 
+import Page from '../components/Page';
 
 function Home() {
-  return <h1>This is the homepage</h1>;
+  return (
+    <Page title="SE: Goal App">
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <Typography variant="h3">Software Engineering</Typography>
+        <Typography variant="h3" sx={{ mb: 5 }}>
+          Goal Management App
+        </Typography>
+        <Typography variant="h3">Team:</Typography>
+        <Typography variant="h3">Hovhannes Chobanyan</Typography>
+        <Typography variant="h3">Erik Minasian</Typography>
+        <Typography variant="h3">Zaruhi Tshitoyan</Typography>
+        <Typography variant="h3">Ani Mkrtumyan</Typography>
+        <Typography variant="h3">Annie Hakian</Typography>
+        <Typography variant="h3">Annabelle Petrosian</Typography>
+      </Box>
+    </Page>
+  );
 }
 
 export default Home;
