c290555eefbcfa00cb5ad2a0549f0ca0febc1792
EMinasian <erikminasian1995@gmail.com>
2022-11-20 17:55:16 +0400

    The structure completed

--- components/goals/NewGoalForm.js
+++ components/goals/NewGoalForm.js
@@ -1,27 +1,20 @@
 import { useRef } from 'react';
-import { FormControl, Button, InputLabel, Input, Box } from '@mui/material';
+import { FormControl, Button, InputLabel, Input, FormGroup, TextField } from '@mui/material';
 
 import Card from '../ui/Card';
-import classes from './NewGoalForm.module.css';
 
 function NewGoalForm(props) {
   const titleInputRef = useRef();
-  const imageInputRef = useRef();
-  const addressInputRef = useRef();
   const descriptionInputRef = useRef();
 
   function submitHandler(event) {
     event.preventDefault();
 
     const enteredTitle = titleInputRef.current.value;
-    const enteredImage = imageInputRef.current.value;
-    const enteredAddress = addressInputRef.current.value;
     const enteredDescription = descriptionInputRef.current.value;
 
     const goalData = {
       title: enteredTitle,
-      image: enteredImage,
-      address: enteredAddress,
       description: enteredDescription,
     };
 
@@ -30,27 +23,19 @@ function NewGoalForm(props) {
 
   return (
     <Card>
-      <form>
+      <FormGroup>
         <FormControl>
-          <InputLabel  htmlFor="title">Meetup Title</InputLabel>
-          <Input type="text" required id="title" ref={titleInputRef} />
-        </FormControl>
-        <FormControl>
-          <InputLabel  htmlFor="image">Meetup Image</InputLabel>
-          <Input type="url" required id="image" ref={imageInputRef} />
-        </FormControl>
-        <FormControl>
-          <InputLabel  htmlFor="address">Address</InputLabel>
-          <input type="text" required id="address" ref={addressInputRef} />
+          <InputLabel  htmlFor="title">Goal Title</InputLabel>
+          <Input type="text" required id="title" inputRef={titleInputRef} />
         </FormControl>
         <FormControl>
           <InputLabel htmlFor="description">Description</InputLabel>
-          <textarea id="description" required rows="5" ref={descriptionInputRef}></textarea>
+          <TextField id="description" required rows="5" inputRef={descriptionInputRef} />
         </FormControl>
         <FormControl>
           <Button type="submit" onClick={submitHandler}>Add goal</Button>
         </FormControl>
-      </form>
+      </FormGroup>
     </Card>
   );
 }
