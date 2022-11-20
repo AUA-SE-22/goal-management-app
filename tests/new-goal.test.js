b84c54657a0b9964d8b3b3e1323569ee3ccd9b81
EMinasian <erikminasian1995@gmail.com>
2022-11-20 22:14:29 +0400

    test added

---
+++ tests/new-goal.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import NewGoal from '../pages/new-goal'
+
+describe('Correctly renders', () => {
+
+    it('the interface to add new goals', () => {
+    const { container } = render(<NewGoal />)
+    const button = screen.getByRole('button', {
+    name: 'Add goal',
+    })
+
+    expect(button).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+    })
+})
