403f93fb96aa0c9ceff3189334e1309bd955052c
EMinasian <erikminasian1995@gmail.com>
2022-12-03 22:44:02 +0400

    redirection logic setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import cookie from 'cookie'
 import PropTypes from 'prop-types';
+import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr';
 import Layout from '../layouts';
 import '../styles/globals.css';
 
@@ -7,12 +9,36 @@ MyApp.propTypes = {
   pageProps: PropTypes.object,
 };
 
-function MyApp({ Component, pageProps }) {
+const keycloakCfg = {
+  realm: 'goal-management',
+  url: 'http://localhost:8181/auth',
+  clientId: 'login-app',
+  login: 'true'
+}
+
+function MyApp({ Component, pageProps, cookies }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SSRKeycloakProvider keycloakConfig={keycloakCfg} persistor={SSRCookies(cookies)}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SSRKeycloakProvider>
   );
+}
+
+
+function parseCookies(req) {
+  if (!req || !req.headers) {
+    return {}
+  }
+  return cookie.parse(req.headers.cookie || '')
+}
+
+MyApp.getInitialProps = async (context) => {
+  // Extract cookies from AppContext
+  return {
+    cookies: parseCookies(context?.ctx?.req)
+  }
 }
 
 export default MyApp;
