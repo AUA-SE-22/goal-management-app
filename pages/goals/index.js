348400b335fa635005d0c2c53da78f5713e2e199
animkrtumyan <animyan@mail.ru>
2022-12-12 02:06:24 +0400

    remove duplicate code for next auth

--- pages/goals/index.js
+++ pages/goals/index.js
@@ -1,12 +1,10 @@
 import GoalList from '../../components/goals/GoalList';
 import Page from '../../components/Page';
-import { useSession, signIn } from 'next-auth/react';
 import GoalManagementService from '../../helpers/service/GoalManagementService';
 import { useEffect, useState } from 'react';
 import { USER_ROLE } from '../../helpers/constants/user';
 
 function Goals() {
-  const { data: token } = useSession();
   const [goals, setGoals] = useState([]);
 
   useEffect(() => {
@@ -21,21 +19,10 @@ function Goals() {
     getGoals();
   }, []);
 
-  if (token) {
-    const accessToken = token.accessToken;
-    const payload = JSON.parse(Buffer.from(accessToken.split('.')[1], 'base64').toString());
-    const role = payload.realm_access.roles[1];
-    return (
-      <Page title="SE: Goal App">
-        <GoalList goals={goals} isEmployer={role === USER_ROLE.employer} />
-      </Page>
-    );
-  }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <Page title="SE: Goal App">
+      <GoalList goals={goals} isEmployer={'role' === USER_ROLE.employer} />
+    </Page>
   );
 }
 
