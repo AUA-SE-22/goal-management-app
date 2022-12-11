2b05fefc32fc45e245c3b955c643a2c11aa7d614
animkrtumyan <animyan@mail.ru>
2022-12-12 02:00:26 +0400

    add auth guard

---
+++ guards/AuthGuard.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+// next
+import { useRouter } from 'next/router';
+// hooks
+import { useSession } from 'next-auth/react';
+import { isValidToken, setSession } from '../utils/jwt';
+// import Login from '../pages/auth/login';
+
+AuthGuard.propTypes = {
+  children: PropTypes.node,
+};
+
+export default function AuthGuard({ children }) {
+  const { data: token } = useSession();
+
+  const { pathname, push } = useRouter();
+
+  const [requestedLocation, setRequestedLocation] = useState(null);
+
+  useEffect(() => {
+    if (requestedLocation && pathname !== requestedLocation) {
+      setRequestedLocation(null);
+      push(requestedLocation);
+    }
+  }, [pathname, push, requestedLocation]);
+
+  if (token) {
+    if (pathname !== requestedLocation) {
+      setRequestedLocation(pathname);
+    }
+    return 'loginpage';
+  }
+
+  const accessToken = token?.accessToken;
+  if (accessToken && isValidToken(accessToken)) {
+    setSession(accessToken);
+    const payload = JSON.parse(Buffer.from(accessToken?.split('.')[1], 'base64').toString());
+    const role = payload.realm_access.roles[1];
+    localStorage.setItem('userRole', role);
+  }
+
+  return <>{children}</>;
+}
